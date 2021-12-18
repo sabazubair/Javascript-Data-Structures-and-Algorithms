003a5013d39c8f478b56a69d4512a82b9bca04d6
Saba Zubair <zubairs19@protonmail.com>
2021-12-17 23:53:52 -0500

    created Node class and LinkedList constructor and in tests, removed skip from a describe block

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,19 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  // executed every time we have a new instance of a node; initialize node
+  // next is default to null; in case it's the last in the chain
+  constructor(data, next = null) {
+    this.data = data; // data is a convention; variable, can be anything
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor(){
+    this.head = null;
+  }
+}
 
 module.exports = { Node, LinkedList };
